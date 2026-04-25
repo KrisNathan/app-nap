7c80ece0650406303e9f8bad8cc156ac87e626e2
KrisNathan <kristophernathanael2005@gmail.com>
2026-04-25 17:27:44 +0700

    feat: basic dbus service registration and listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use libc::pid_t;
+use std::{error::Error, future::pending};
+use zbus::{connection, interface};
+
+struct Daemon {}
+
+#[interface(name = "dev.appnap.AppNap1")]
+impl Daemon {
+    fn update_window(&self, window_id: &str, pid: pid_t, active: bool) -> String {
+        let buf = format!(
+            "update_window: window_id={} pid={} active={}",
+            window_id, pid, active
+        );
+        println!("{}", buf);
+        buf
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let daemon = Daemon {};
+    let _conn = connection::Builder::session()?
+        .name("dev.appnap.AppNap")?
+        .serve_at("/dev/appnap/AppNap", daemon)?
+        .build()
+        .await?;
+
+    pending::<()>().await;
+    Ok(())
 }

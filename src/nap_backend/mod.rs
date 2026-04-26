7e48982e9c56bec32c9eaf2d13f8b197a4cf9e04
KrisNathan <kristophernathanael2005@gmail.com>
2026-04-26 11:10:43 +0700

    refactor(nap-backend): reorganize backend module exports

--- src/nap_backend/mod.rs
+++ src/nap_backend/mod.rs
@@ -1,5 +1,7 @@
 mod backend;
 mod system_signal_backend;
+mod systemd_scope_quota_backend;
 
 pub use backend::NapBackend;
 pub use system_signal_backend::SystemSignalController;
+pub use systemd_scope_quota_backend::SystemdScopeQuotaBackend;
